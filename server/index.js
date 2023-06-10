d2a8bbe017e1d75c70f9b13d9c10591f7762d2af
nidhihiwse <hiwse.nid@gmail.com>
2023-06-10 10:29:34 +0530

    changes for glitch

--- server/index.js
+++ server/index.js
@@ -16,14 +16,23 @@ app.use(cors());
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.get('/', async (request, response) => {
+  response.sendFile(__dirname + '/views/index.html');
+})
+
 app.post('/send', async (request, response) => {
   let data = request.body;
+  console.log(data)
   let user = await checkIfUserExists(data.id)
-
   if (user) { // user is present and registered
-    response.send(await isUserUnderAge(data, user, 10))    
+    let ageRes = await isUserUnderAge(data, user, 10);
+    if (ageRes === "overAge") {
+      response.sendFile(__dirname + '/views/error-overage.html');
+    } else {
+      response.sendFile(__dirname + '/views/message-sent.html');
+    }   
   } else {
-    response.send("unregistered");
+    response.sendFile(__dirname + '/views/error-unregistered.html');
   }
 });
 
@@ -31,7 +40,6 @@ app.post('/send', async (request, response) => {
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
 
 // start mail sender every 15 seconds
 setInterval(sendEmail, 15000);
